e956d95c07bf8fac416f826e2235401f240aace6
Sabyrzhan Zhaslanuly <realsabyrka@outlook.com>
2026-06-18 19:37:20 +0500

    feat: style AppHeader for desktop

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -24,18 +24,18 @@ const menuItems = [
 </script>
 
 <template>
-  <header>
-    <RouterLink to="/">
-      <img :src="logoImage" alt="Vue Sneakers Logo" />
+  <header class="flex justify-between items-center p-10">
+    <RouterLink class="flex items-center gap-4" to="/">
+      <img class="w-10" :src="logoImage" alt="Vue Sneakers Logo" />
       <div>
-        <h1>Vue Sneakers</h1>
-        <p>The Best Sneakers Shop</p>
+        <h1 class="text-xl font-bold uppercase">Vue Sneakers</h1>
+        <p class="text-sm text-[#9D9D9D]">The Best Sneakers Shop</p>
       </div>
     </RouterLink>
     <nav>
-      <ul>
+      <ul class="flex items-center gap-8">
         <li v-for="item in menuItems" :key="item.href">
-          <RouterLink :to="item.href">
+          <RouterLink class="flex items-center gap-2" :to="item.href">
             <img :src="item.icon" alt="" />
             <span>{{ item.label }}</span>
           </RouterLink>
