5c11a401d07393c84b1e23318bd66b23b29db51b
Sabyrzhan Zhaslanuly <realsabyrka@outlook.com>
2026-06-18 17:53:14 +0500

    feat: install and configure tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
-
-<style scoped></style>
