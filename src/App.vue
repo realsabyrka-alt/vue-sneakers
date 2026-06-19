281f449eb34f6df0b8cb8a4f36d895e2eb51ef23
Sabyrzhan Zhaslanuly <realsabyrka@outlook.com>
2026-06-19 11:28:07 +0500

    feat: configure vue router and setup views

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="bg-white w-11/12 h-screen mx-auto my-20 rounded-xl shadow-xl 2xl:w-4/5">
+    <AppHeader />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('@/views/CartView.vue'),
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: () => import('@/views/FavoritesView.vue'),
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('@/views/ProfileView.vue'),
+    },
+  ],
 })
 
 export default router
